accd969a925dfff450db2e47bce056c4c3d9a4eb
Thomas Reyes <thomasellison.r@gmail.com>
2017-11-01 09:59:52 -0700

    rebuilt lab up to yesterday's point. Changed db name in appsettings.json

---
+++ Week8Tom/Week8Tom/Controllers/HeroesController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Week8Tom.Data;
+using Week8Tom.Models;
+
+namespace Week8Tom.Controllers
+{
+    //Route token & attribute routing
+    [Route("api/[controller]")]
+    public class HeroesController : ControllerBase
+    {
+        private readonly HeroStatsDbContext _context;
+
+        //Dependency injection of context
+        public HeroesController(HeroStatsDbContext context)
+        {
+            _context = context;
+        }
+
+        //GET
+        //Id constraint
+        [HttpGet("{id:int?}")]
+        public IActionResult Get(int id)
+        {
+            //linq query using a lambda expression
+            var result = _context.HeroStats.FirstOrDefault(h => h.Id == id);
+
+            //returns 200 status code
+            return Ok(result);
+        }
+
+        //POST
+        //Model binding
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Post([FromBody]HeroStats stat)
+        {
+            await _context.AddAsync(stat);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("Get", new { id = stat.Id }, stat);
+        }
+
+        //PUT
+
+        //DELETE
+    }
+}
---
+++ Week8Tom/Week8Tom/Models/HeroStats.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Week8Tom.Models
+{
+    public class HeroStats
+    {
+        public int Id { get; set; }
+        public string Alias { get; set; }
+        public string Name { get; set; }
+
+    }
+}
