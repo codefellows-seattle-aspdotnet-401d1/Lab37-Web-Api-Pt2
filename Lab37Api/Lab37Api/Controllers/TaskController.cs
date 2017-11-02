c7442a189224db8cad06f47539e7ad2600ee7fe0
duane rush <desertduane@yahoo.com>
2017-11-01 21:16:38 -0700

    comments and readme

--- Lab37Api/Lab37Api/Controllers/TaskController.cs
+++ Lab37Api/Lab37Api/Controllers/TaskController.cs
@@ -8,16 +8,18 @@ using System.Threading.Tasks;
 
 namespace Lab37Api.Controllers
 {
+    //Custom route with route token
     [Route("api/[Controller]")]
     public class TaskController : ControllerBase
     {
         private readonly BirthdayPlanDbContext _context;
-
+        // dependency injection for DbContext
         public TaskController(BirthdayPlanDbContext context)
         {
             _context = context;
         }
 
+        //Get method for a single item from the Db
         [HttpGet("{id:int}")]
         public IActionResult Get(int id)
         {
@@ -25,6 +27,7 @@ namespace Lab37Api.Controllers
             return Ok(result);
         }
 
+        //Http Get method to retrieve all items
         [HttpGet]
         [Produces("application/json")]
         public IEnumerable<BirthdayPlan> Get()
@@ -32,6 +35,7 @@ namespace Lab37Api.Controllers
             return _context.BirthdayPlan;
         }
 
+        //Post method to add a new item
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] BirthdayPlan plan)
         {
@@ -41,29 +45,36 @@ namespace Lab37Api.Controllers
             return CreatedAtAction("Get", new { id = plan.ID }, plan);
         }
 
+        //Put method to update an item
         [HttpPut("{id:int}")]
         public async Task<IActionResult> Put(int id, [FromBody] BirthdayPlan plan)
         {
             if (!ModelState.IsValid)
             {
+                //returns a BadRequest with a message for what part of the ModelState was invalid
                 return BadRequest(ModelState);
             }
             var Put = _context.BirthdayPlan.FirstOrDefault(p => p.ID == id);
+
             if (Put != null)
             {
+                //Replaces the data in the variable with the new data brought in
                 Put.Task = plan.Task;
                 Put.IsComplete = plan.IsComplete;
                 _context.Update(Put);
 
                 await _context.SaveChangesAsync();
                 return Ok();
+
             }
+            
             else
             {
                 return BadRequest();
             }
         }
 
+        //Deletes the data with the id that is brought in
         [HttpDelete("{id:int}")]
         public async Task<IActionResult>Delete(int id)
         {
--- README.md
+++ README.md
@@ -3,9 +3,17 @@
 ## ReadMe
 - Your readme should include the following information:
 	- How long did it take you to complete this assignment?
+	3 hours took this long due to starting over and taking my time.
+
 	- What did you struggle with? Why? How did you solve?
+	No struggle just worked my way back through the build using the demo when needed to 
+	progress forward.
+
 	- What did you learn during this assignment?
+	Learned more about implementing the RESTful methods for a web api
+
     - What resources did you utilize for this assingment?
+	The demo code and the Web api reading microsoft docs.
 
 ## Rubric
 - 4pts: 5 actions (Get (all), Get(id), Post, Put, Delete) properly implemented and working
