c6b48e7235c5e3c2ccf3832e4ad8d5d7fad6d894
George Gaylor <georgegaylor83@outlook.com>
2017-11-01 23:35:48 -0700

    Inital project and startup

---
+++ Lab37George/Lab37George/Startup.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lab37George
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // adding mvc
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            // adding mvc
+            app.UseMvc();
+
+            app.Run(async (context) =>
+            {
+                // Added text to the default page since there is no front end for the API
+                await context.Response.WriteAsync("This is the parts API default page.");
+            });
+        }
+    }
+}
